7395fe4c2b1d183256fb288257270054887d7700
Elliot Wilens <wilensel@gmail.com>
2020-09-11 14:35:59 -0500

    Add supplier_image_upload.py files (lab & local versions)

---
+++ supplier_image_upload_local.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import requests
+import glob
+
+image_dir = "C:/Users/Elliot/Python/Google/capstone/supplier-data/images/" # update to linux path
+images = glob.glob(image_dir + '*')
+
+url = 'http://localhost/upload/'
+
+def upload_images(image_files):
+  for image in images:
+    print("Uploading image: {}...".format(image))
+    with open(image, 'rb') as f:
+        response = requests.post(url, files={'file':f})
+        print(response.status_code, ' ', response.reason)
+
+def main():
+  upload_images(images)
+
+if __name__ == '__main__':
+  main()
