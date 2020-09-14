7f4479ed10f2b3a918de4815275c5f0e38b78285
Elliot Wilens <wilensel@gmail.com>
2020-09-14 12:12:36 -0500

    Update emails script to work for emails w/o attachment
    This is the first time I'm using *args so revert if it doesn't work.

--- emails_lab.py
+++ emails_lab.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 
-from reports import generate_report
 import email.message
 import mimetypes
 import os.path
 import smtplib
 
-def generate_email(sender, recipient, subject, body, attachment_path):
+def generate_email(sender, recipient, subject, body, *attachment_path):
     """Creates an email with an attachement."""
     message = email.message.EmailMessage()
     message["From"] = sender
     message["To"] = recipient
     message["Subject"] = subject
     message.set_content(body)
-    # process the attachment w/ it's mimetype & attach to email
-    attachment_filename = os.path.basename(attachment_path)
-    mime_type, _ = mimetypes.guess_type(attachment_path)
-    mime_type, mime_subtype = mime_type.split('/', 1)
-    with open(attachment_path, 'rb') as ap:
-        message.add_attachment(ap.read(),
-        maintype=mime_type,
-        subtype=mime_subtype,
-        filename=attachment_filename)
+    # if there's an attachment, process it & add it to the message
+    for attachment in attachment_path:
+        # process the attachment w/ it's mimetype & attach to email
+        attachment_filename = os.path.basename(attachment)
+        mime_type, _ = mimetypes.guess_type(attachment)
+        mime_type, mime_subtype = mime_type.split('/', 1)
+        with open(attachment, 'rb') as ap:
+            message.add_attachment(ap.read(),
+            maintype=mime_type,
+            subtype=mime_subtype,
+            filename=attachment_filename)
     return message
 
 def send_email(message):
