d57b1f1edbb810a9f7f06dc48afe4f4101edba29
Elliot Wilens <wilensel@gmail.com>
2020-09-14 14:09:18 -0500

    Update emails.py to add a second function for emails w/o attachments

--- emails.py
+++ emails.py
@@ -5,25 +5,30 @@ import mimetypes
 import os.path
 import smtplib
 
-def generate_email(sender, recipient, subject, body, *attachment_path):
+def generate_email(sender, recipient, subject, body, attachment_path):
     """Creates an email with an attachement."""
     message = email.message.EmailMessage()
     message["From"] = sender
     message["To"] = recipient
     message["Subject"] = subject
     message.set_content(body)
-    # if there's an attachment, process it & add it to the message
-    # the for-loop is needed in order for this function to support emails w/o attachments
-    for attachment in attachment_path:
-        # process the attachment w/ it's mimetype & attach to email
-        attachment_filename = os.path.basename(attachment)
-        mime_type, _ = mimetypes.guess_type(attachment)
-        mime_type, mime_subtype = mime_type.split('/', 1)
-        with open(attachment, 'rb') as ap:
-            message.add_attachment(ap.read(),
-            maintype=mime_type,
-            subtype=mime_subtype,
-            filename=attachment_filename)
+    attachment_filename = os.path.basename(attachment_path)
+    mime_type, _ = mimetypes.guess_type(attachment_path)
+    mime_type, mime_subtype = mime_type.split('/', 1)
+    with open(attachment_path, 'rb') as ap:
+        message.add_attachment(ap.read(),
+        maintype=mime_type,
+        subtype=mime_subtype,
+        filename=attachment_filename)
+    return message
+
+def generate_alert_email(sender, recipient, subject, body):
+    """Creates an email without an attachement."""
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
     return message
 
 def send_email(message):
